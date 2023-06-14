e15f12cb878d29e0492f8a58a9454a73151a1aa5
Amshu G M <amshugm2003@gmail.com>
2023-06-14 21:18:13 +0530

    Added basic layout along with navbar

---
+++ club_activity_page/src/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return ( 
+        <div className="navbar">
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/87/BMS_College_of_Engineering.svg/1200px-BMS_College_of_Engineering.svg.png" alt="bmsce logo" />
+            <p>BMSCE</p>
+            <ul>
+                
+                <li><a href="#">Home</a></li>
+                <li><a href="#">NavPage</a></li>
+                <li><a href="#">profile</a></li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Header;
